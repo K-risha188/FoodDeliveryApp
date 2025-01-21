f34fc9b81de202ae27ce86e3b939ce593fe348da
K-risha188 <krisharathod1645@gmail.com>
2025-01-21 19:26:51 +0530

    Initial Commit

---
+++ FoodDeliveryApp/FoodDeliveryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodDeliveryAppApp.swift
+//  FoodDeliveryApp
+//
+//  Created by student on 21/01/25.
+//
+
+import SwiftUI
+
+@main
+struct FoodDeliveryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            WelcomeView()
+        }
+    }
+}
