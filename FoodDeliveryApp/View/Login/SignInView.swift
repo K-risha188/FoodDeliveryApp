f8d0154c86296778c6be8abea4c2f712e8c9111b
K-risha188 <krisharathod1645@gmail.com>
2025-01-22 19:44:30 +0530

    Update

--- FoodDeliveryApp/View/Login/SignInView.swift
+++ FoodDeliveryApp/View/Login/SignInView.swift
@@ -9,13 +9,58 @@ import SwiftUI
 
 struct SignInView: View {
     var body: some View {
-        Image("sign_in_top")
-            .resizable()
-            .frame(width:500)
-        
+        NavigationStack{
+            ZStack{
+                Image("bottom_bg")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: .screenWidth,height: .screenHeight)
+                VStack{
+                    Image("sign_in_top")
+                        .resizable()
+                        .scaledToFill()
+                        .padding(.bottom,500)
+                    
+                }
+                
+                VStack{
+                    Text("Get your groceries \n with nectar")
+                        .font(.customfont(.semibold, fontSize: 30))
+                        .foregroundColor(.black)
+                        .multilineTextAlignment(.leading)
+                        .padding(.trailing,80)
+                }
+                VStack{
+                    Text("or connect with social media")
+                        .font(.customfont(.medium, fontSize: 15))
+                        .foregroundColor(.black.opacity(0.5))
+                        .padding(.top,350)
+                    NavigationLink{
+                        EmptyView()
+                    }
+                label:
+                    {
+                        HStack{
+                            Text("Continue with Google")
+                                .font(.customfont(.semibold, fontSize: 18))
+                                .foregroundColor(.white)
+                                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 60, maxHeight: 60)
+                                .background(Color.blue)
+                                .cornerRadius(20)
+                                .padding()
+                                .padding(.horizontal)
+                        }
+                    }
+                }
+                
+            }
+            .ignoresSafeArea()
+        }
     }
 }
 
+    
 #Preview {
     SignInView()
+    
 }
--- FoodDeliveryApp/View/Login/WelcomeView.swift
+++ FoodDeliveryApp/View/Login/WelcomeView.swift
@@ -24,31 +24,38 @@ struct WelcomeView: View {
                         .resizable()
                         .frame(width: 60,height:60)
                         .scaledToFit()
-                        .padding()
-                    Text("Welcome")
+                        .padding(.bottom)
+                    Text("Welcome\n to our store")
 //                        .font(.largeTitle)
                         .font(.customfont(.semibold, fontSize: 57))
-                        .bold()
                         .foregroundColor(.white)
-                    Text("to our store")
-                        .font(.largeTitle)
-                        .bold()
-                        .foregroundColor(.white)
+                        .multilineTextAlignment(.center)
+                    
                     Text("Get your groceries in as fast as one hour")
-                        .foregroundColor(.white)
+                        .foregroundColor(.white.opacity(0.5))
+                        .font(.customfont(.medium ,fontSize: 15))
+                        .multilineTextAlignment(.center)
+                        .padding(.bottom)
                     
                     
                     NavigationLink{
                         SignInView()
-                    } label: {
+                    }
+                    label: {
                         Text("Get started")
                             .font(.customfont(.semibold, fontSize: 18))
                             .foregroundColor(.white)
                             .frame(minWidth: 0, maxWidth: .infinity, minHeight: 60, maxHeight: 60)
                             .background(Color.primaryApp)
-                            .cornerRadius(10)
+                            .cornerRadius(20)
                     }
-                    .padding(.horizontal, 10)
+                    .padding(.horizontal,30)
+                    .navigationTitle("")
+                    .navigationBarBackButtonHidden(true)
+                    .navigationBarHidden(true)
+                    .ignoresSafeArea()
+                        
+                    
                 }
         
                 .padding(.bottom,90)
@@ -57,6 +64,7 @@ struct WelcomeView: View {
         .navigationTitle("")
         .navigationBarBackButtonHidden(true)
         .navigationBarHidden(true)
+        .ignoresSafeArea()
     }
 }
 
