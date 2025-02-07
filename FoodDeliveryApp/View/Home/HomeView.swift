3406183e446f2e99d8537e87ff7a1ad28c5618f1
K-risha188 <krisharathod1645@gmail.com>
2025-02-07 19:25:49 +0530

    Update

--- FoodDeliveryApp/View/Home/HomeView.swift
+++ FoodDeliveryApp/View/Home/HomeView.swift
@@ -43,12 +43,12 @@ struct HomeView: View{
                         .padding(.trailing,30)
                     Button{
                     }
-                        label:{
-                            Text("See all")
-                                .font(.customfont(.regular, fontSize: 15))
-                                .foregroundColor(.green)
-                                .padding(.top,28)
-                                .padding(.leading,120)
+                    label:{
+                        Text("See all")
+                            .font(.customfont(.regular, fontSize: 15))
+                            .foregroundColor(.green)
+                            .padding(.top,28)
+                            .padding(.leading,120)
                         
                     }
                 }
@@ -56,61 +56,109 @@ struct HomeView: View{
                 
                 ScrollView{
                     VStack{
-                        VStack{
-                            Image("banana")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 120)
-                            
-                            Text("Organic Bananas")
-                                .font(.customfont(.semibold, fontSize:18))
-                                .padding(.top,20)
-                                .multilineTextAlignment(.leading)
-                            
-                            Text("7pcs, Priceg")
-                                .font(.customfont(.regular, fontSize:18))
-                                .foregroundColor(.black.opacity(0.4))
-                                .padding(.top,5)
-                                .multilineTextAlignment(.trailing)
-                                .frame(alignment: .leading)
-                            
+                        ScrollView(.horizontal, showsIndicators: false){
                             HStack{
-                                Text("$4.99")
-                                    .font(.customfont(.semibold, fontSize:20))
-                                    .foregroundColor(.black.opacity(0.8))
-                                    .padding(.top,40)
-                             
-                                    RoundedRectangle(cornerRadius: 15)
-                                        .fill(Color.primaryApp)    .frame(width: 50, height: 50)
-                                        .overlay(
-                                            Image("add")
+                                RoundedRectangle(cornerRadius: 15)
+                                    .stroke(Color.gray.opacity(0.3), lineWidth: 1)
+                                    .frame(width: 180, height: 300)
+                                    .padding(.top,10)
+                                    .overlay(
+                                        VStack{
+                                            Image("banana")
                                                 .resizable()
                                                 .scaledToFit()
-                                                .frame(width: 20, height: 20)
-                                                .foregroundColor(.primaryApp)
-                                        )
-                                        .padding(.leading,50)
-                                        .padding(.top,35)
-                             
+                                                .frame(width: 120)
+                                            
+                                            Text("Organic Bananas")
+                                                .font(.customfont(.semibold, fontSize:18))
+                                                .padding(.top,20)
+                                                .padding(.trailing,10)
+                                            
+                                            Text("7pcs, Priceg")
+                                                .font(.customfont(.regular, fontSize:18))
+                                                .foregroundColor(.black.opacity(0.4))
+                                                .padding(.top,5)
+                                                .multilineTextAlignment(.trailing)
+                                                .frame(alignment: .leading)
+                                                .padding(.trailing,50)
+                                            
+                                            HStack{
+                                                Text("$4.99")
+                                                    .font(.customfont(.semibold, fontSize:20))
+                                                    .foregroundColor(.black.opacity(0.8))
+                                                    .padding(.top,40)
+                                                
+                                                RoundedRectangle(cornerRadius: 20)
+                                                    .fill(Color.primaryApp)
+                                                    .frame(width: 50, height: 50)
+                                                    .overlay(
+                                                        Image("add")
+                                                            .resizable()
+                                                            .scaledToFit()
+                                                            .frame(width: 20, height: 20)
+                                                            .foregroundColor(.primaryApp)
+                                                    )
+                                                    .padding(.leading,50)
+                                                    .padding(.top,35)
+                                                
+                                            }
+                                        }
+                                    )
+                                    .padding()
+                                
+                                RoundedRectangle(cornerRadius: 15)
+                                    .stroke(Color.gray.opacity(0.3), lineWidth: 1)
+                                    .frame(width: 180, height: 300)
+                                    .padding(.top,10)
+                                    .overlay(
+                                        VStack{
+                                            Image("apple")
+                                                .resizable()
+                                                .scaledToFit()
+                                                .frame(width: 135)
+                                            
+                                            Text("Red Apple")
+                                                .font(.customfont(.semibold, fontSize:18))
+                                                .padding(.top,30)
+                                                .multilineTextAlignment(.trailing)
+                                            
+                                            Text("1kg, Priceg")
+                                                .font(.customfont(.regular, fontSize:18))
+                                                .foregroundColor(.black.opacity(0.4))
+                                                .padding(.top,5)
+                                                .multilineTextAlignment(.trailing)
+                                                .frame(alignment: .leading)
+                                            
+                                            HStack{
+                                                Text("$4.99")
+                                                    .font(.customfont(.semibold, fontSize:20))
+                                                    .foregroundColor(.black.opacity(0.8))
+                                                    .padding(.top,40)
+                                                
+                                                RoundedRectangle(cornerRadius: 20)
+                                                    .fill(Color.primaryApp)
+                                                    .frame(width: 50, height: 50)
+                                                    .overlay(
+                                                        Image("add")
+                                                            .resizable()
+                                                            .scaledToFit()
+                                                            .frame(width: 20, height: 20)
+                                                            .foregroundColor(.primaryApp)
+                                                    )
+                                                    .padding(.leading,50)
+                                                    .padding(.top,35)
+                                                
+                                            }
+                                        }
+                                    )
                             }
-                            
                         }
                     }
-                    .padding()
-                    .frame(width: 200)
-                    .background(Color.white)
-                    .cornerRadius(15)
-                    .overlay(
-                            RoundedRectangle(cornerRadius: 15)
-                                .stroke(Color.gray.opacity(0.3), lineWidth: 1)
-                                .padding(.horizontal,5))
                 }
             }
         }
     }
 }
-
-#Preview {
-    HomeView()
-}
-
+    #Preview {
+        HomeView()
+    }
