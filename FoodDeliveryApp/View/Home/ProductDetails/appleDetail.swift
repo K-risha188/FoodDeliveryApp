c5c7cd239647261c6be0d75d152f492d205d5ef9
K-risha188 <krisharathod1645@gmail.com>
2025-02-19 19:11:49 +0530

    Updates in appleDetail page

--- FoodDeliveryApp/View/Home/ProductDetails/appleDetail.swift
+++ FoodDeliveryApp/View/Home/ProductDetails/appleDetail.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct appleDetail: View {
+    @State private var count = 1
     var body: some View {
         NavigationStack {
             ScrollView{
@@ -29,7 +30,7 @@ struct appleDetail: View {
                     }
                     HStack{
                         Text("Natural Red Apple")
-                            .font(.customfont(.semibold, fontSize:25))
+                            .font(.customfont(.bold, fontSize:25))
                             .padding(.trailing,100)
                         
                         Image("fav")
@@ -37,15 +38,62 @@ struct appleDetail: View {
                             .scaledToFit()
                             .frame(width: 25, height: 25)
                     }
+                    
                     Text("1kg, Price")
                         .font(.customfont(.regular, fontSize: 20))
                         .opacity(0.5)
                         .padding(.trailing,260)
                     
                     HStack {
-                       //Create clickable add and subtrack button
+                        Button(action: {
+                            count -= 1
+                        }) {
+                            Image("subtack")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 25, height: 25)
+                                .padding()
+                        }
+                        
+                        RoundedRectangle(cornerRadius:20)
+                            .stroke(Color.gray.opacity(0.3), lineWidth: 2)
+                            .frame(width: 50, height: 50)
+                            .overlay(
+                                Text("\(count)")
+                                    .font(.customfont(.semibold, fontSize:20))
+                            )
+                        
+                        Button(action: {
+                            count += 1
+                        }) {
+                            Image("add_green")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 25, height: 25)
+                                .padding()
+                        }
+                        
+                        Text("$4.99")
+                            .font(.customfont(.semibold, fontSize:20))
+                            .padding(.leading, 120)
                     }
+                    .padding(.trailing, 15)
+                    .padding(.top,30)
+                    
+                    Divider()
+                        .frame(width:350)
+                        .background(Color.gray.opacity(0.3))
+                        .padding(.top,10)
                 }
+                
+                HStack{
+                    Text("Product Detail")
+                        .font(.customfont(.semibold, fontSize: 20))
+                        .opacity(0.85)
+                    
+                    //Continue from Dropdown details button
+                }
+                .padding(.top,15)
             }
         }
     }
